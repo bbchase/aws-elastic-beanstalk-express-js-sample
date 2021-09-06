b1c09b793bc08d1a18ecb09abd151227a84feb34
Ben Chase <bbchase@Monolith.local>
2021-09-06 18:05:07 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hail and well met!'));
+app.get('/', (req, res) => res.send('Now we\'re getting somewhere!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
